880ddbeeda9e080aa8dccd76d4937ec0d63f9631
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-01-26 01:25:05 +0530

    EX3 ADD OUT

--- 1-typescript-casestudies/ex-03.ts
+++ 1-typescript-casestudies/ex-03.ts
@@ -7,3 +7,8 @@ const weatherReport = (city:string,temperature:number,isRaining:boolean) => {
 }
 
 weatherReport(city,temperature,isRaining)
+
+// ❯ bun tsr ex-03.ts
+
+// $ bun x tsc--noEmit && bun run "ex-03.ts"
+// In trichy, it is 40°C.Is it raining ? false
