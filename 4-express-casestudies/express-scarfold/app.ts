3f0f79a70bde8bbe5c9809d85b8229849e12aeaa
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-02-26 20:26:23 +0530

    reset

--- 4-express-casestudies/express-scarfold/app.ts
+++ 4-express-casestudies/express-scarfold/app.ts
@@ -1,13 +1,8 @@
 import express, { type Request, type Response } from 'express'
 
-import contactRouter from "./routes/contact";
-import productRouter from "./routes/product";
-import pointsRouter from "./routes/points";
+
 
 const app = express()
 
-app.use("/contact", contactRouter);
-app.use("/product", productRouter); 
-app.use("/points", pointsRouter);
 
 export default app;
