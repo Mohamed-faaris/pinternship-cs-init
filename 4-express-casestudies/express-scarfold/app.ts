91b35c30bd147e9bbb3f95cd8614e8eba305e19d
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-02-26 19:52:52 +0530

    ex3

--- 4-express-casestudies/express-scarfold/app.ts
+++ 4-express-casestudies/express-scarfold/app.ts
@@ -1,9 +1,11 @@
 import express, { type Request, type Response } from 'express'
 
 import contactRouter from "./routes/contact";
+import productRouter from "./routes/product";
 
 const app = express()
 
 app.use("/contact", contactRouter);
+app.use("/product", productRouter);
 
 export default app;
