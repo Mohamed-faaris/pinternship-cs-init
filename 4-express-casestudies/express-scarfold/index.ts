524911ac8f010ff06a524cf9252d9e4e0f2fa321
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-02-26 16:26:28 +0530

    ex-01

--- 4-express-casestudies/express-scarfold/index.ts
+++ 4-express-casestudies/express-scarfold/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from 'express'
+
+const PORT = 3000
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to Greenfield Community Center!');
+});
+
+app.get('/events', (req: Request, res: Response) => {
+    const events = [
+        'Yoga Class - Monday 7pm',
+        'Gardening Workshop - Wednesday 5pm',
+        'Book Club - Friday 6pm'
+    ];
+    res.json(events);
+});
+
+app.get('/contact', (req: Request, res: Response) => {
+    const contactInfo = {
+        email:"me@faaris.dev",
+        phone:"1231231231"
+    }
+    res.json(contactInfo);
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
